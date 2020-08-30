25d8cb47ecdc419bfd6a515e791c043d62fc6a18
Neda Maikstenaite <Neda.Maikstenaite@euromonitor.com>
2020-08-30 22:54:55 +0300

    Completed task

---
+++ cachematrix.R
@@ -0,0 +1,34 @@
+## Put comments here that give an overall description of what your
+## functions do
+
+## Write a short comment describing this function
+
+makeCacheMatrix <- function(x = matrix()) {
+  inversem <- NULL
+  set <- function(y) {
+    x <<- y
+    inversem <<- NULL
+  }
+  get <- function() x
+  setinversem <- function(inverse) inversem <<- inverse
+  getinversem <- function() inversem
+  list(set = set, get = get,
+       setinversem = setinversem,
+       getinversem = getinversem)
+}
+
+
+## Write a short comment describing this function
+
+cacheSolve <- function(x, ...) {
+        ## Return a matrix that is the inverse of 'x'
+  inversem <- x$getinversem()
+  if(!is.null(inversem)) {
+    message("getting cached data")
+    return(inversem)
+  }
+  data <- x$get()
+  inversem <- solve(data, ...)
+  x$setinversem(inversem)
+  inversem
+}
